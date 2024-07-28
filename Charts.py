94e395b42b564191a7e0b3d8fbf1c54129a2eed4
Sampreeth-Rayas <107186172+Sampreeth-Rayas@users.noreply.github.com>
2024-07-28 19:15:08 +0530

    Create different charts
    Has functions to create surface, polar and scatter charts.

---
+++ Charts.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+
+class Charts:
+    def plot_surface_chart(self):
+        return 0
+
+    def plot_polar_chart(self):
+        return 0
+
+    def plot_scatter_chart(self):
+        return 0
+
+    def clear_charts(self):
+        return 0
