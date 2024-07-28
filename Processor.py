9edd2be289aee245602e15cc9f5873e48b66c039
Sampreeth-Rayas <107186172+Sampreeth-Rayas@users.noreply.github.com>
2024-07-28 19:13:48 +0530

    Add files via upload
    Contains functions to populate complex number array, populate chirp data for all 4 channels, sum channel data, 1 dimension FFT, 2 dimension FFT and creates absolute data array

---
+++ Processor.py
@@ -0,0 +1,27 @@
+
+class Processor:
+
+    def Processor(self):
+        # All the below class variables are two dim array with dim 128 x 256
+        self.fft_data = []
+        self.two_dim_fft = []
+        self.abs_fft = []
+        self.two_dim_fft = []
+
+    def sum_channel_data(self):
+        return 0
+
+    def perform_fft(self):
+        return  0
+
+    def perform_2D_fft(self):
+        return 0
+
+    def get_filtered_data(self):
+        return 0
+
+    def get_abs_data_fft(self):
+        return 0
+
+    def get_abs_data_2D_fft(self):
+        return 0
